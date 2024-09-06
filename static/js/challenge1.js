d2014615e3d3c6414ed10cabcc029806e94acb5a
Michael Foley <michaelfoley1999@gmail.com>
2024-09-06 17:04:00 -0400

    made some funky DOM manipulation just for shits and giggles

--- static/js/challenge1.js
+++ static/js/challenge1.js
@@ -1,5 +1,12 @@
 function clickMe(){
-    document.getElementById("challenge1-output").innerText="Clicked!";
+    
+    let age = prompt("How old are you?");
+    let ageInDays = parseInt(age) * 365;
+    let h1Output = document.createElement("h1");
+    let textOutput = document.createTextNode("You are " + ageInDays + " days old!");
+    h1Output.appendChild(textOutput);
+    document.getElementById("challenge1-output").innerHTML = h1Output.outerHTML;
+
 }
 
 function reset(){
